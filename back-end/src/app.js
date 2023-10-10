77d5c42085c068cc637449904ef4586ebd1e1e0e
stable-release <stable-release@users.noreply.github.com>
2023-10-10 00:33:16 -0500

    Access-Control-Allow-Headers added to response header

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,6 +4,7 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 
+// Uncomment for development logging
 // const logger = require("./utils/logger");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -13,8 +14,19 @@ const tablesRouter = require("./routes/tables/tables.router");
 
 const app = express();
 
+// Uncomment for development logging
 // app.use(logger);
 app.use(express.json());
+
+// Set Access-Control-Allow-Origin headers to prevent no-cors behavior
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 app.use("/reservations", reservationsRouter);
 app.use("/tables", tablesRouter);
